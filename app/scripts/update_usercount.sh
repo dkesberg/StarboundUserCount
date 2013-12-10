8fa45f65b55a54a1e8d6ee20b11335325b9b1975
Daniel Kesberg <dkesberg@gmail.com>
2013-12-10 11:27:20 +0100

    Fixed path

--- app/scripts/update_usercount.sh
+++ app/scripts/update_usercount.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-lsof -i TCP:21025|grep ESTABLISHED|wc -l > /var/www/starbound/app/storage/statistics/usercount.txt
+lsof -i TCP:21025|grep ESTABLISHED|wc -l > /var/www/starbound/app/storage/data/usercount.txt
 
 # alternative
-# netstat -a | grep :21025 | grep ESTABLISHED | wc -l > /var/www/starbound/app/storage/statistics/usercount.txt
+# netstat -a | grep :21025 | grep ESTABLISHED | wc -l > /var/www/starbound/app/storage/data/usercount.txt
